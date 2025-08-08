c86c1d4c0e7462dd195e4d97a4e61c2b18b69b75
Tamara Inbar <tamarainbar@gmail.com>
2025-08-08 10:49:02 -0400

    can add new birthdays

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,15 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Sara", birthday: .now),
+        Friend(name: "Jenna", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    
+    
     var body: some View {
-        VStack {
-            
-            
-            
-            
-            
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+                
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom){
+                VStack (alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                    
+                }
+                .padding()
+                .background(.bar)
+                
+            }
         }
-        .padding()
+            
     }
 }
 
