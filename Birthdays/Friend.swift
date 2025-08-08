61bc018af0afcac415cf29215da101d5e9af2f6c
Tamara Inbar <tamarainbar@gmail.com>
2025-08-08 09:51:52 -0400

    created swift file and made a friend templete

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
